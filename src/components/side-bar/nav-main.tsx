8359c95ac4eb4701b5a9d1569b63925f0d46d351
Vimit Dhawan <vimit0601@live.com>
2025-12-20 15:42:07 +0700

    remove quick booking button

--- src/components/side-bar/nav-main.tsx
+++ src/components/side-bar/nav-main.tsx
@@ -23,16 +23,7 @@ export function NavMain() {
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
         <SidebarMenu className="w-48">
-          <SidebarMenuItem className="flex items-center gap-2">
-            <SidebarMenuButton
-              tooltip="Quick Booking"
-              className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear cursor-pointer"
-              onClick={() => router.push("/bookings/create")}
-            >
-              <IconCirclePlusFilled />
-              <span>Quick Booking</span>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
+
         </SidebarMenu>
         <SidebarMenu>
           {data.map((item) => (
